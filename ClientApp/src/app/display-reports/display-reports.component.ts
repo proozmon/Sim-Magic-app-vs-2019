81161bc1cfb2b200e4b2f9cf5462bed346ea6d33
Pierre Roozmon <proozmon@rogers.com>
2020-05-02 22:47:35 -0400

    Simplify the 'Display reports' component, in anticipation of adding the 'Display analytics' component.

--- ClientApp/src/app/display-reports/display-reports.component.ts
+++ ClientApp/src/app/display-reports/display-reports.component.ts
@@ -11,9 +11,6 @@ export class DisplayReportsComponent implements OnInit {
   myValueDisplayOperatingCosts: boolean;
   myValueDisplayNetWorth: boolean;
   myValueDisplayRiskAnalysis: boolean;
-  myValueDisplayDescriptiveResults: boolean;
-  myValueDisplayPredictiveResults: boolean;
-  myValueDisplayPrescriptiveRecommendations: boolean;
 
   constructor() { }
 
@@ -22,9 +19,6 @@ export class DisplayReportsComponent implements OnInit {
     this.myValueDisplayOperatingCosts = false;
     this.myValueDisplayNetWorth = false;
     this.myValueDisplayRiskAnalysis = false;
-    this.myValueDisplayDescriptiveResults = false;
-    this.myValueDisplayPredictiveResults = false;
-    this.myValueDisplayPrescriptiveRecommendations = false;
   }
 
   showActive(tab: number) {
@@ -35,63 +29,24 @@ export class DisplayReportsComponent implements OnInit {
         this.myValueDisplayOperatingCosts = false;
         this.myValueDisplayNetWorth = false;
         this.myValueDisplayRiskAnalysis = false;
-        this.myValueDisplayDescriptiveResults = false;
-        this.myValueDisplayPredictiveResults = false;
-        this.myValueDisplayPrescriptiveRecommendations = false;
         break;
       case 1:
         this.myValueHowToDisplayReports = false;
         this.myValueDisplayOperatingCosts = true;
         this.myValueDisplayNetWorth = false;
         this.myValueDisplayRiskAnalysis = false;
-        this.myValueDisplayDescriptiveResults = false;
-        this.myValueDisplayPredictiveResults = false;
-        this.myValueDisplayPrescriptiveRecommendations = false;
         break;
       case 2:
         this.myValueHowToDisplayReports = false;
         this.myValueDisplayOperatingCosts = false;
         this.myValueDisplayNetWorth = true;
         this.myValueDisplayRiskAnalysis = false;
-        this.myValueDisplayDescriptiveResults = false;
-        this.myValueDisplayPredictiveResults = false;
-        this.myValueDisplayPrescriptiveRecommendations = false;
         break;
       case 3:
         this.myValueHowToDisplayReports = false;
         this.myValueDisplayOperatingCosts = false;
         this.myValueDisplayNetWorth = false;
         this.myValueDisplayRiskAnalysis = true;
-        this.myValueDisplayDescriptiveResults = false;
-        this.myValueDisplayPredictiveResults = false;
-        this.myValueDisplayPrescriptiveRecommendations = false;
-        break;
-      case 4:
-        this.myValueHowToDisplayReports = false;
-        this.myValueDisplayOperatingCosts = false;
-        this.myValueDisplayNetWorth = false;
-        this.myValueDisplayRiskAnalysis = false;
-        this.myValueDisplayDescriptiveResults = true;
-        this.myValueDisplayPredictiveResults = false;
-        this.myValueDisplayPrescriptiveRecommendations = false;
-        break;
-      case 5:
-        this.myValueHowToDisplayReports = false;
-        this.myValueDisplayOperatingCosts = false;
-        this.myValueDisplayNetWorth = false;
-        this.myValueDisplayRiskAnalysis = false;
-        this.myValueDisplayDescriptiveResults = false;
-        this.myValueDisplayPredictiveResults = true;
-        this.myValueDisplayPrescriptiveRecommendations = false;
-        break;
-      case 6:
-        this.myValueHowToDisplayReports = false;
-        this.myValueDisplayOperatingCosts = false;
-        this.myValueDisplayNetWorth = false;
-        this.myValueDisplayRiskAnalysis = false;
-        this.myValueDisplayDescriptiveResults = false;
-        this.myValueDisplayPredictiveResults = false;
-        this.myValueDisplayPrescriptiveRecommendations = true;
         break;
       default:
         break;
