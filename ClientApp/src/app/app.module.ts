1d75e327b4e9f52075420a13036b1e3d735cbfd2
Pierre Roozmon <proozmon@rogers.com>
2020-05-02 21:12:52 -0400

    Change the 'Display results' components to 'Display reports',as the first step in adding components for display Analytics and Simulations

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -9,7 +9,7 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 import { WorkingOnSimulationsComponent } from './working-on-simulations/working-on-simulations.component';
-import { DisplayResultsComponent } from './display-results/display-results.component';
+import { DisplayReportsComponent } from './display-reports/display-reports.component';
 
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
@@ -23,7 +23,7 @@ import { map, catchError } from 'rxjs/operators';
     HomeComponent,
     WelcomePageComponent,
     WorkingOnSimulationsComponent,
-    DisplayResultsComponent,
+    DisplayReportsComponent,
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -35,7 +35,7 @@ import { map, catchError } from 'rxjs/operators';
       { path: '', component: WelcomePageComponent, pathMatch: 'full' },
       { path: 'welcome-page', component: WelcomePageComponent },
       { path: 'working-on-simulations', component: WorkingOnSimulationsComponent },
-      { path: 'display-results', component: DisplayResultsComponent },
+      { path: 'display-reports', component: DisplayReportsComponent },
     ])
   ],
   providers: [],
